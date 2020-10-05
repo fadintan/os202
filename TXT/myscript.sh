00023060daec875f0bf9e4dc9ec7da0f52cc9849
Intan Fadilla A <fadintan@gmail.com>
2020-10-05 22:00:48 +0700

    Update myscript.sh

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -3,22 +3,48 @@
 # Mon Sep 28 22:03:27 WIB 2020
 # Tue 29 Sep 2020 11:02:39 AM WIB
 
+filename='myrank.txt'
 FILES="mylog.txt mypubkey.txt myrank.txt myscript.sh"
 SHA="SHA256SUM"
 
-rm -f $SHA $SHA.asc
+echo "Menu"
+echo "[1] Add MyRank"
+echo "[2] Sign Files"
 
-echo "sha256sum $FILES > $SHA"
-sha256sum $FILES > $SHA
+read -p ">> Enter your choice: " opt
 
-echo "sha256sum -c $SHA"
-sha256sum -c $SHA
+if [ $(($OPTION)) -eq 1 ]
+	then
+    echo "I'll help you update the Top 3 Rank to myrank.txt~"
+    echo "You just need to fill in this form and i'll do the magic! :D"
 
-echo "gpg -o $SHA.asc -a -sb $SHA"
-gpg -o $SHA.asc -a -sb $SHA
+    # Masukkan input minggu dan id github
+    read -p ">> Enter Week: " newweek
+    read -p ">> Enter your 1st Top 3: " first
+    read -p ">> Enter your 2nd Top3: " second
+    read -p ">> Enter your 3rd Top3: " third
 
-echo "gpg --verify $SHA.asc $SHA"
-gpg --verify $SHA.asc $SHA
+    # Append variables ke file myrank.txt
+    printf "ZCZC $newweek $first\n" >> $filename
+    printf "ZCZC $newweek $second\n" >> $filename
+    printf "ZCZC $newweek $third\n" >> $filename
+    echo "Rank Updated!"
+  
+elif [ $(($OPTION)) -eq 2 ]
+	then
+    rm -f $SHA $SHA.asc
+
+    echo "sha256sum $FILES > $SHA"
+    sha256sum $FILES > $SHA
+
+    echo "sha256sum -c $SHA"
+    sha256sum -c $SHA
+
+    echo "gpg -o $SHA.asc -a -sb $SHA"
+    gpg -o $SHA.asc -a -sb $SHA
+
+    echo "gpg --verify $SHA.asc $SHA"
+    gpg --verify $SHA.asc $SHA
 
 exit 0
 
